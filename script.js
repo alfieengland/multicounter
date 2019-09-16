9626e11a16eab7641afdfcfdfdb762ceeaa73de0
Alfie England <alfieengland@live.co.uk>
2019-09-16 17:17:18 +0100

    Added footer section containing total sum of all counters

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var counterButtons;
 var btnAddCounter;
-var counters = 0;
+var counters = 0; // Number of counters active/onscreen, initialised as 0
 var counterTemplate;
+var labelTotal; // Label element "labelTotal" which shows the total count of all counters
+var countTotal = 0; // Int of combined counters' amounts
 
 function afterPageLoads() {
-  counterButtons = document.getElementsByClassName('counterButton');
   btnAddCounter = document.getElementById('addCounter');
+  labelTotal = document.getElementById('labelTotal');
   btnAddCounter.addEventListener('click', addCounter);
 }
 
@@ -14,18 +15,24 @@ function addCounter() {
   var clone = counterTemplate.content.cloneNode(true);
   document.body.appendChild(clone); // Add an instance HTML elements/contents present withing counterTemplate to the page
   //counterButtons[counters].addEventListener('click', countFunction);
-  count[counters] = 0; // Add new value in array of "count" in which the count of the newly created "counter" is stored
   counters += 1; // Add one to the total number of "counters" current present on the page
 }
 
 function countFunction(elem) {
   var currentValue = parseInt(elem.innerHTML) + 1;
   elem.innerHTML = currentValue;
+  countersTotal();
 }
 
 document.addEventListener('DOMContentLoaded', afterPageLoads); /* Triggers the function after the page has loaded */
 
 // Confirmation alert/popup on reload to prevent user from accidentally leaving page & losing counters
 window.onbeforeunload = function() {
-     return "";
- };
+  return "";
+};
+
+// Update "Total" count label
+function countersTotal() {
+  countTotal += 1;
+  labelTotal.innerHTML = "Total: " + countTotal;
+}
